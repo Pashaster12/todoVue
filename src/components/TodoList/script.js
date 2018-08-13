18aa7d553a7c8dfe4cc73003145a7439976943ee
Pavel Basenko <p.basenko@simcord.com>
2018-08-13 19:38:00 +0300

    Separated the single file component to three different files with HTML, JS and CSS.

--- src/components/TodoList/script.js
+++ src/components/TodoList/script.js
@@ -1,37 +1,3 @@
-<template>
-  <div>
-    <h1>{{ title }}</h1>
-    <input 
-      v-model="itemText"
-      @keydown.enter="saveItem()"
-      placeholder="Add or edit the todo item text..."
-    >
-
-    <h4>For saving todo item text just press Enter button...</h4>
-
-    <p 
-      v-show="!items.length"
-      :style="pstyle"
-    >
-      Todo list is empty :(
-    </p>
-
-    <ol>
-      <li
-        v-for="item in items"
-        :key="item.id"
-      >
-        {{ item.text }}
-        <span class="item-controls">
-          <button @click="editItem(item)">Edit</button>
-          <button @click="deleteItem(item.id)">Delete</button>
-        </span>
-      </li>
-    </ol>
-  </div>
-</template>
-
-<script>
 export default {
   name: "TodoList",
   data() {
@@ -84,21 +50,3 @@ export default {
     }
   }
 };
-</script>
-
-<style>
-h1 {
-  text-align: center;
-}
-input {
-  width: 100%;
-  padding: 8px 10px;
-  border: 1px solid #000;
-}
-li {
-  padding-bottom: 10px;
-}
-.item-controls {
-  float: right;
-}
-</style>
---
+++ src/components/TodoList/template.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <input 
+      v-model="itemText"
+      @keydown.enter="saveItem()"
+      placeholder="Add or edit the todo item text..."
+    >
+
+    <h4>For saving todo item text just press Enter button...</h4>
+
+    <p 
+      v-show="!items.length"
+      :style="pstyle"
+    >
+      Todo list is empty :(
+    </p>
+
+    <ol>
+      <li
+        v-for="item in items"
+        :key="item.id"
+      >
+        {{ item.text }}
+        <span class="item-controls">
+          <button @click="editItem(item)">Edit</button>
+          <button @click="deleteItem(item.id)">Delete</button>
+        </span>
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script src="./script.js"></script>
+<style src="./style.css"></style>
